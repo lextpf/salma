0af3114a851684352eb8e52cf99a6f40917eb571
Lextpf <74869642+alexandertoepfer@users.noreply.github.com>
2026-07-12 21:30:00 +0200

    ♻️ Route fomods to unified LibraryPage and comps imports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import ErrorBoundary from './components/ErrorBoundary'
+import Layout from './comps/Layout'
+import ErrorBoundary from './comps/ErrorBoundary'
 import InstallPage from './InstallPage'
-import FomodBrowserPage from './FomodBrowserPage'
-import FomodDetailPage from './FomodDetailPage'
+import LibraryPage from './LibraryPage'
 import LogsPage from './LogsPage'
 import SettingsPage from './SettingsPage'
 
@@ -13,8 +12,8 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<InstallPage />} />
-          <Route path="fomods" element={<FomodBrowserPage />} />
-          <Route path="fomods/:name" element={<FomodDetailPage />} />
+          <Route path="fomods" element={<LibraryPage />} />
+          <Route path="fomods/:name" element={<LibraryPage />} />
           <Route path="logs" element={<LogsPage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>
